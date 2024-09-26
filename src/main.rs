5484c197e79fde93e5f1504be22411e1c8df05b1
bigwhoman <hm.keshvari@gmail.com>
2024-09-26 18:42:26 +0000

    added some fix

--- src/main.rs
+++ src/main.rs
@@ -36,26 +36,34 @@ fn start_game(mut board: [[char; ROW_SIZE]; COL_SIZE]) {
     let mut player = 'B';
     let map: HashMap<char, usize> = ('a'..='h').zip(0..8).collect();
     loop {
-        let mut input_line: String = Default::default();
-        print!("Enter move for colour {} (RowCol): ", player);
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut input_line).expect("Wrong input");
+        loop {
+            let mut input_line: String = Default::default();
+            print!("Enter move for colour {} (RowCol): ", player);
 
-        let row_move: char = input_line.trim().chars().nth(0).unwrap();
-        let col_move: char = input_line.trim().chars().nth(1).unwrap();
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input_line).expect("Wrong input");
+            print_board(board);
+            let row_move: char = input_line.trim().chars().nth(0).unwrap();
+            let col_move: char = input_line.trim().chars().nth(1).unwrap();
 
-        if let (Some(&row), Some(&col)) = (map.get(&row_move), map.get(&col_move)) {
-            if board[row][col] == '.' {
-                board[row][col] = player;
-                do_move(&mut board, row, col, player);
+            if let (Some(&row), Some(&col)) = (map.get(&row_move), map.get(&col_move)) {
+                if board[row][col] == '.' {
+                    board[row][col] = player;
+                    let valid_move: bool = do_move(&mut board, row, col, player);
+                    if valid_move {
+                        break;
+                    }
+                    board[row][col] = '.';
+                } else {
+                    println!("Invalid Move");
+                }
             } else {
-                println!("Invalid Move");
+                println!("Invalid move");
             }
-        } else {
-            println!("Invalid move");
+
+            println!("Invalid Move. Try again.")
         }
 
-        print_board(board);
         if player == 'B' {
             player = 'W';
         } else {
@@ -71,7 +79,8 @@ fn start_game(mut board: [[char; ROW_SIZE]; COL_SIZE]) {
 /*
     The logic core of the game. Pretty easy I might say
 */
-fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, player:char) {
+fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, player: char) -> bool {
+    let mut valid_move: bool = false;
     for i in [-1_i32, 0, 1] {
         for j in [-1_i32, 0, 1] {
             let mut temp_row: i32 = row as i32;
@@ -79,21 +88,25 @@ fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, pla
             if i == 0 && j == 0 {
                 continue;
             } else {
-                while ['W', 'B'].iter().any(|&x| x == board[temp_row.abs() as usize][temp_col.abs() as usize]) {
+                while ['W', 'B']
+                    .iter()
+                    .filter(|&&x| x != player)
+                    .any(|&x| x == board[(temp_row + i).abs() as usize][(temp_col + j).abs() as usize])
+                {
                     // println!("col {} row : {}",temp_col, temp_row);
 
-                    temp_row += i ;
-                    temp_col += j ;
-                    println!("tmp row {} col : {}",temp_row, temp_col);
-                    println!("i {} and j {}", i, j);
+                    temp_row += i; 
+                    // println!("tmp row {} col : {}", temp_row, temp_col);
+                    // println!("i {} and j {}", i, j);
                     if board[temp_row.abs() as usize][temp_col.abs() as usize] == player {
-                        println!("nooooooogi");
-                        println!("lampppppppppppp row {} col : {}",temp_row, temp_col);
-                        println!("jampppppp row {} col : {}",row as i32, col as i32);
+                        valid_move = true;
+                        // println!("nooooooogi");
+                        // println!("lampppppppppppp row {} col : {}", temp_row, temp_col);
+                        // println!("jampppppp row {} col : {}", row as i32, col as i32);
                         while temp_row != row as i32 || temp_col != col as i32 {
-                            temp_col -= j ;
-                            temp_row -= i ;
-                            
+                            temp_col -= j;
+                            temp_row -= i;
+
                             println!("shooo gi ---> row {} col{}", temp_row, temp_col);
                             board[temp_row.abs() as usize][temp_col.abs() as usize] = player;
                         }
@@ -104,6 +117,7 @@ fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, pla
         }
     }
     // print_board(*board);
+    return valid_move;
 }
 
 /*
@@ -113,7 +127,6 @@ fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, pla
 fn check_for_end() -> bool {
     return true;
 }
-
 
 /*
     Simple function to print your board
