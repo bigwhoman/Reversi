cc6a561516952d51217ff565abff7a4441e33016
bigwhoman <hm.keshvari@gmail.com>
2024-09-26 17:52:13 +0000

    added the core logic

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 }
 
 fn game() {
-    let mut board = initialize();
+    let board = initialize();
     // board[1][2] = 2;
     // print_board(board);
     // board[2][3] = 5;
@@ -21,6 +21,10 @@ fn game() {
 // Initial things -- Num of rows and columns
 fn initialize() -> [[char; ROW_SIZE]; COL_SIZE] {
     let mut board: [[char; ROW_SIZE]; COL_SIZE] = [['.'; ROW_SIZE]; COL_SIZE];
+    board[3][3] = 'B';
+    board[4][4] = 'B';
+    board[3][4] = 'W';
+    board[4][3] = 'W';
 
     return board;
 }
@@ -38,14 +42,19 @@ fn start_game(mut board: [[char; ROW_SIZE]; COL_SIZE]) {
         let col_move: char = input_line.trim().chars().nth(1).unwrap();
 
         if let (Some(&row), Some(&col)) = (map.get(&row_move), map.get(&col_move)) {
-            board[row][col] = player;
+            if board[row][col] == '.' {
+                board[row][col] = player;
+                do_move(&mut board, row, col, player);
+            } else {
+                println!("Invalid Move");
+            }
         } else {
             println!("Invalid move");
         }
 
         print_board(board);
         if player == 'B' {
-            player = 'A';
+            player = 'W';
         } else {
             player = 'B';
         }
@@ -53,9 +62,43 @@ fn start_game(mut board: [[char; ROW_SIZE]; COL_SIZE]) {
             // break;
         }
     }
-    println!("Game has ended and {} has won!!!", 12);
+    // println!("Game has ended and {} has won!!!", 12);
 }
 
+fn do_move(board: &mut [[char; ROW_SIZE]; COL_SIZE], row: usize, col: usize, player:char) {
+    for i in [-1_i32, 0, 1] {
+        for j in [-1_i32, 0, 1] {
+            let mut temp_row: i32 = row as i32;
+            let mut temp_col: i32 = col as i32;
+            if i == 0 && j == 0 {
+                continue;
+            } else {
+                while ['W', 'B'].iter().any(|&x| x == board[temp_row.abs() as usize][temp_col.abs() as usize]) {
+                    // println!("col {} row : {}",temp_col, temp_row);
+
+                    temp_row += i ;
+                    temp_col += j ;
+                    println!("tmp row {} col : {}",temp_row, temp_col);
+                    println!("i {} and j {}", i, j);
+                    if board[temp_row.abs() as usize][temp_col.abs() as usize] == player {
+                        println!("nooooooogi");
+                        println!("lampppppppppppp row {} col : {}",temp_row, temp_col);
+                        println!("jampppppp row {} col : {}",row as i32, col as i32);
+                        while temp_row != row as i32 || temp_col != col as i32 {
+                            temp_col -= j ;
+                            temp_row -= i ;
+                            
+                            println!("shooo gi ---> row {} col{}", temp_row, temp_col);
+                            board[temp_row.abs() as usize][temp_col.abs() as usize] = player;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // print_board(*board);
+}
 fn check_for_end() -> bool {
     return true;
 }
